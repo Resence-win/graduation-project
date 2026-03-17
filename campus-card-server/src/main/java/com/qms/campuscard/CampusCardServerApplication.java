bbbf4bc3e7b481319eb905fb62e094bb5f8fc621
Resence-win <ieligk123@gmail.com>
2026-03-17 19:29:26 +0800

    后端提交

---
+++ campus-card-server/src/main/java/com/qms/campuscard/CampusCardServerApplication.java
@@ -0,0 +1,13 @@
+package com.qms.campuscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusCardServerApplication.class, args);
+    }
+}
+
